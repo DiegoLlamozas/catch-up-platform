1df1c888a35740832e032c9573cf07afbe30018f
alumnos <diegollamozas911@gmail.com>
2025-10-24 16:18:11 -0500

    feat(repository): add UnitOfWork implementation for managing database transactions

---
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,14 @@
+using CatchUpPlatform.API.Shared.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+    
+    
+}
